3fa323b4384ed7a374786c1e37faa1f4b3244a45
LJWSW <1029701949@qq.com>
2023-12-12 14:30:19 +0800

    暂时提交

---
+++ HarmonyOS03/entry/src/main/ets/pages/ArrayPage.js
@@ -0,0 +1,12 @@
+"use strict";
+let arr1 = ['鸿蒙os', 'flutter', 'golang'];
+let arr2 = [1, 23, 45, 65];
+struct ArrayPage extends   {
+    constructor() { }
+    build() {
+        .height('100%')
+            .width('100%')
+            .padding(10);
+    }
+}
+//# sourceMappingURL=ArrayPage.js.map
---
+++ HarmonyOS03/entry/src/main/ets/pages/Index.js
@@ -0,0 +1,48 @@
+"use strict";
+// 数字类型 (全局变量 组件中使用全局变量不需要加this)
+let num1 = 123;
+let num2 = 12.3;
+let num3 = 0x1f;
+let num4 = 0o12;
+let num5 = 66;
+// 字符串类型
+let str1 = 'HarmonyOS Next不支持Android应用了';
+let str2 = 'ArkTS';
+let str3 = `马总今年${num5}岁了`;
+// 布尔类型
+let flag1 = true;
+let flag2 = false;
+// 联合类型
+let a;
+a = 12;
+a = 'str';
+a = true;
+// 枚举类型：使用枚举类型可以为一组数值赋予友好的名字
+var myColor;
+(function (myColor) {
+    myColor["RED"] = "red";
+    myColor["BLUE"] = "blue";
+})(myColor || (myColor = {}));
+let content = myColor.RED;
+// Unknown any : 表示的是任意类型
+let c;
+c = 12;
+c = true;
+c = '666';
+let d;
+d = 12;
+d = true;
+d = '666';
+// 代码格式化： ctrl + alt + l
+struct Index extends   {
+    constructor(
+// 数字类型 (全局变量 组件中使用全局变量不需要加this)
+    // 数字类型 (全局变量 组件中使用全局变量不需要加this)
+    ) {
+        // 数字类型 (全局变量 组件中使用全局变量不需要加this)
+    }
+    build() {
+        .height('100%');
+    }
+}
+//# sourceMappingURL=Index.js.map
